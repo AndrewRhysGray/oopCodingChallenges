825df46ec2f4796cc23a6cbbd0a414ae85cdbe08
Andrew Gray <andrew.rhysgray@hotmail.co.uk>
2024-02-26 18:33:46 +0000

    sayHello() functioning, moving onto the Polymorphism challenges

--- oopCodingTask3.js
+++ oopCodingTask3.js
@@ -1,63 +1,69 @@
-// Coding Task 3 - Inheritance (Subclass and Superclass)
-// In this task your are required to create an Admin class, which is a child class of the User  class:
+// // Coding Task 3 - Inheritance (Subclass and Superclass)
+// // In this task your are required to create an Admin class, which is a child class of the User  class:
 
-// 1. Create a  User class:
-// § Add to the class a property with the name of username.
-class User {
-    constructor(username) {
-      this.numberOfArticles = 0;
-      this._username = username;
-    }
+// // 1. Create a  User class:
+// // § Add to the class a property with the name of username.
+// class User {
+//     constructor(username) {
+//       this.numberOfArticles = 0;
+//       this._username = username;
+//     }
     
    
   
   
-// § Create a setter method that can set the value of the username.
+// // § Create a setter method that can set the value of the username.
 
-set username(newUsername) {
-    this._username = 'Andy';
-  }
-}
+// set username(newUsername) {
+//     this._username = 'Andy';
+//   }
+// }
 
-// 2. Create the Admin class that inherits the User class:
-class Admin extends User {
-    constructor (username) {
-        super(username);
-        this.username = username;
-    }
-    expressYourRole() {
-        return "Admin";
-    }
-    sayHello() {
-        return "Hello admin ", admin
-    }
-}
+// // 2. Create the Admin class that inherits the User class:
+// class Admin extends User {
+//     constructor (username) {
+//         super(username);
+//         this.username = username;
+//     }
+//     expressYourRole() {
+//         return "Admin";
+//     }
+//     sayHello() {
+//         return "Hello admin " + this._username;
+//     }
+// }
+// const admin = new Admin("David John")
+// const role = admin.expressYourRole();
+// const helloSayer = admin.sayHello();
 
-const admin = new Admin("David John")
-const role = admin.expressYourRole();
-helloSayer = admin.sayHello()
+// // console.log(role);
+// // console.log(helloSayer);
 
-// console.log(role)
-// console.log(helloSayer)
-// § Add a method by the name of expressYourRole(), and make it return the string: "Admin".
+// // § Add a method by the name of expressYourRole(), and make it return the string: "Admin".
 
+// // § Add to the Admin class another method, sayHello(), that returns the string "Hello admin, XXX" with the username instead of XXX.
 
-// § Add to the Admin class another method, sayHello(), that returns the string "Hello admin, XXX" with the username instead of XXX.
+// console.log(role);
+// console.log(helloSayer);
 
 // 3. Create an object admin out of the class Admin:
 // § Set its name to "Balthazar" and say hello to the user.
 
 
+
+
 // Coding Task 4 - Polymorphism
 // In order to implement the polymorphism principle, you will create a User class that commits the classes that inherit from it to calculate the number of scores that a user has depending on the number of articles that he has authored or edited.
 
-// On the basis of the User class, we are going to create the Author and Editor classes, and both will calculate the number of scores with the method calcScores(), although the calculated value will differ between the two classes.
+// On the basis of the User class, we are going to c
+//reate the Author and Editor classes, and both will
+// calculate the number of scores with the method 
+//calcScores(), although the calculated value wi
+//ll differ between the two classes.
 
 // This is the skeleton for the User class:
 
 // Example:
-
-
 // 1. Add to the User class the methods to set and get the number of articles:
 
 // setNumberOfArticles(int numberOfArticles)
