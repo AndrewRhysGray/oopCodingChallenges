8ae106a19fdb121cb43bbcf4af806f93eaf644a3
Andrew Gray <andrew.rhysgray@hotmail.co.uk>
2024-02-27 16:28:47 +0000

    Coding challenges complete, I rewrite this frequently for revision

--- oopCodingTask3.js
+++ oopCodingTask3.js
@@ -1,123 +1,86 @@
-// // Coding Task 3 - Inheritance (Subclass and Superclass)
-// // In this task your are required to create an Admin class, which is a child class of the User  class:
+class User {
+    constructor(username) {
+        this.numberOfArticles = 0;
+        this._username = username;
+    }
 
-// // 1. Create a  User class:
-// // § Add to the class a property with the name of username.
-// class User {
-//     constructor(username) {
-//       this.numberOfArticles = 0;
-//       this._username = username;
-//     }
-    
-   
-  
-  
-// // § Create a setter method that can set the value of the username.
+    set username(newUsername) {
+        this._username = newUsername;
+    }
 
-// set username(newUsername) {
-//     this._username = 'Andy';
-//   }
-// }
+    setNumberOfArticles(numberOfArticles) {
+        this.numberOfArticles = numberOfArticles;
+    }
 
-// // 2. Create the Admin class that inherits the User class:
-// class Admin extends User {
-//     constructor (username) {
-//         super(username);
-//         this.username = username;
-//     }
-//     expressYourRole() {
-//         return "Admin";
-//     }
-//     sayHello() {
-//         return "Hello admin " + this._username;
-//     }
-// }
-// const admin = new Admin("David John")
-// const role = admin.expressYourRole();
-// const helloSayer = admin.sayHello();
+    getNumberOfArticles() {
+        return this.numberOfArticles;
+    }
 
-// // console.log(role);
-// // console.log(helloSayer);
+    calcScores() {
+        // This method will be implemented differently in Author and Editor classes
+    }
 
-// // § Add a method by the name of expressYourRole(), and make it return the string: "Admin".
+    expressYourRole() {
+        return "User";
+    }
 
-// // § Add to the Admin class another method, sayHello(), that returns the string "Hello admin, XXX" with the username instead of XXX.
+    sayHello() {
+        return "Hello " + this.expressYourRole() + " " + this._username;
+    }
+}
 
-// console.log(role);
-// console.log(helloSayer);
+class Author extends User {
+    calcScores() {
+        return this.numberOfArticles * 10 + 20;
+    }
+}
 
-// 3. Create an object admin out of the class Admin:
-// § Set its name to "Balthazar" and say hello to the user.
+class Editor extends User {
+    calcScores() {
+        return this.numberOfArticles * 6 + 15;
+    }
+}
 
+const author = new Author("Balthazar");
+author.setNumberOfArticles(8);
+console.log("Author's scores:", author.calcScores());
 
+const editor = new Editor("Melchior");
+editor.setNumberOfArticles(15);
+console.log("Editor's scores:", editor.calcScores());
 
+class UserAbstract {
+    constructor(username) {
+        this._username = username;
+    }
 
-// Coding Task 4 - Polymorphism
-// In order to implement the polymorphism principle, you will create a User class that commits the classes that inherit from it to calculate the number of scores that a user has depending on the number of articles that he has authored or edited.
+    set username(newUsername) {
+        this._username = newUsername;
+    }
 
-// On the basis of the User class, we are going to c
-//reate the Author and Editor classes, and both will
-// calculate the number of scores with the method 
-//calcScores(), although the calculated value wi
-//ll differ between the two classes.
+    get username() {
+        return this._username;
+    }
 
-// This is the skeleton for the User class:
+    stateYourRole() {
+        throw new Error("This method must be implemented by subclasses");
+    }
+}
 
-// Example:
-// 1. Add to the User class the methods to set and get the number of articles:
+class Admin extends UserAbstract {
+    stateYourRole() {
+        return "admin";
+    }
+}
 
-// setNumberOfArticles(int numberOfArticles)
-// getNumberOfArticles() 
+class Viewer extends UserAbstract {
+    stateYourRole() {
+        return "viewer";
+    }
+}
 
+const admin = new Admin("Balthazar")
+const viewer = new Viewer("Melchior")
 
-// 2. Add to the User class a third method: 
-
-// calcScores(), that performs the scores calculations separately for each class.
-
-
-// 3. Create an Author class that inherits from the User class. 
-
-// In the Author create a calcScores() method that returns the number of scores from the following calculation: 
-
-// numberOfArticles * 10 + 20
-
-
-// 4. Create an Editor class that inherits from the User class. 
-
-// In the Editor create a calcScores() method that returns the number of scores from the following calculation: 
-
-// numberOfArticles * 6 + 15
-
-
-// 5. Create an object, author, from the Author class, set the number of articles to 8, and print the scores that the author gained.
-
-
-
-// 6. Create another object, editor, from the Editor class, set the number of articles to 15, and print the scores that the editor gained.
-
-
-// Coding Task 5 - Abstraction
-// In this task , we will create an abstract User class and two child classes (Admin and Viewer classes) that inherit from the abstract class.
-
-// 1. Create an abstract class with the name of User: 
-
-// Add to the class, a property with the name of username. 
-// Add to the class, an abstract method with the name of stateYourRole().
-// Add to the class, the setter and getter methods to set and get the username.
-
-
-// 2. Create an Admin class that inherits the User abstract class: 
-
-// Add to the class a method stateYourRole() and let it return the string "admin".
-
-
-// 3. Create another class, Viewer that inherits the User abstract class: 
-
-// Add to the class a method stateYourRole() and let it return the string "viewer".
-
-
-// 4. Create an object, admin, from the Admin class, set the username to "Balthazar", and make it return the string "admin".
-
-
-
-// 5. Create an object, viewer, from the Viewer class, set the username to "Melchior", and make it return the string "viewer".
+console.log(admin.username, admin.stateYourRole())
+console.log(viewer.username, viewer.stateYourRole());
